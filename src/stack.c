b5487a943aa5cfcd90e12b6703b133d2d6d5a653
Berin Smaldon <bms508@york.ac.uk>
2011-12-15 19:21:27 +0000

    Laid groundworks for stack - doesn't yet do anything

---
+++ src/stack.c
@@ -0,0 +1,22 @@
+#include "afternoon1.h"
+#include "microcode.h"
+#include "stack.h"
+
+void compute_stack(Afternoon1 *state, Afternoon1 *next)
+{
+    if (control_line(state, W0))
+    {
+    }
+
+    if (control_line(state, W1))
+    {
+    }
+
+    if (control_line(state, W2))
+    {
+    }
+
+    if (control_line(state, W3))
+    {
+    }
+}
