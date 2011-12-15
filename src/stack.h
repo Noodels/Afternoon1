6652042b45712dd7b77f0bcea75cdaa24e35672e
Berin Smaldon <bms508@york.ac.uk>
2011-12-15 19:47:34 +0000

    Header file for stack

---
+++ src/stack.h
@@ -0,0 +1,5 @@
+/* Compute Stack
+ * Reads read and write lines for the stack and shifts all data accordingly
+ * Note: Uses main_data_input of first operand, the computation of which
+ *       MUST precede the computation of the stack */
+void compute_stack(Afternoon1*, Afternoon1*);
