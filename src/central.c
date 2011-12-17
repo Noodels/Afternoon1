0b76c4fedb41ccaaf7236ad932bcda841ce2290e
Berin Smaldon <bms508@york.ac.uk>
2011-12-17 15:43:00 +0000

    Extending central code to be more useful

--- src/central.c
+++ src/central.c
@@ -3,7 +3,6 @@
 #include "afternoon1.h"
 #include "stack.h"
 #include "memory.h"
-//#include "microcode.h"
 #include "cache.h"
 #include "blu.h"
 
@@ -18,12 +17,14 @@ int afternoon1_setup(Afternoon1 *new, int stacksize, int cachesize)
 
     new->carry_register = 0;
 
+    free(new->stackptr);
     new->stackptr = calloc(1, stacksize);
     new->stack_size = stacksize;
       
     new->current_address = 0;
     new->memory_request = 0;
 
+    free(new->cache);
     new->cache = calloc(2, cachesize); // 1 - 16
     new->cache_size = cachesize;
 
@@ -33,6 +34,23 @@ int afternoon1_setup(Afternoon1 *new, int stacksize, int cachesize)
     new->DATAOUT = 0;
 
     if ((new->cache == NULL) || (new->stackptr == NULL))
+        return -1;
+    else
+        return 0;
+}
+
+int afternoon1_ghostcopy(Afternoon1 *src, Afternoon1 *dst)
+{
+    free(dst->cache);
+    free(dst->stackptr);
+
+    dst->stack_size = src->cache_size;
+    dst->stackptr = calloc(1, src->stack_size);
+
+    dst->cache_size = src->cache_size;
+    dst->cache = calloc(2, src->cache_size);
+
+    if ((dst->cache == NULL) || (dst->stackptr == NULL))
         return -1;
     else
         return 0;
@@ -53,4 +71,38 @@ void afternoon1_step(Afternoon1 *state, Afternoon1 *next)
     compute_memaccess(state, next);
     compute_microcode(state, next);
     compute_stack(state, next);
+}
+
+void afternoon1_cycles(Afternoon1 *source, Afternoon1 *inter,
+        Afternoon1 *dest, int n)
+{
+    Afternoon1 *t;
+    int i;
+    
+    if (n % 2)
+    {   // if odd no. of cycles, ensure dest is written to first
+        // this will guarantee that dest is written to last
+        // 1: dest
+        // 3: dest, inter, dest
+        // 5: dest, inter, dest, inter, dest
+        t = inter;
+        inter = dest;
+        dest = t;
+    }
+        // if even no. of cycles, inter is written to first
+        // 2: inter, dest
+        // 4: inter, dest, inter, dest
+
+    // initial source to inter step, leaving (n-1) cycles to do
+    afternoon1_step(source, inter);
+
+    for (i=1; i<n; i++)
+    {
+        afternoon1_step(inter, dest);
+
+        // And swap around
+        t = inter;
+        inter = dest;
+        dest = t;
+    }
 }
--- src/central.h
+++ src/central.h
@@ -5,6 +5,16 @@
 int afternoon1_setup(Afternoon1*, int, int);
 void afternoon1_free(Afternoon1*); // deinitialise
 
+/* Initialises the second value in such a way that it may store results from
+ * the source argument */
+int afternoon1_ghostcopy(Afternoon1*, Afternoon1*);
+
 /* Wrapper for all the afternoon1 computation and bus control
  * Takes a source and a destination state of processor */
 void afternoon1_step(Afternoon1*, Afternoon1*);
+
+/* Wrapper for stepping between states, performs a given number
+ * of cycles, preserving the first argument after one initial
+ * read, mangling the second argument, and writing to the last
+ * argument */
+void afternoon1_cycles(Afternoon1*, Afternoon1*, Afternoon1*, int);
